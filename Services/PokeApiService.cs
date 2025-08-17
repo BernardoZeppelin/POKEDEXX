54fae2196c80d41dd9e39fbf04c18495d1f70a0e
BernardoZeppelin <160980763+BernardoZeppelin@users.noreply.github.com>
2025-08-17 11:50:02 -0300

    Add files via upload

---
+++ Services/PokeApiService.cs
@@ -0,0 +1,45 @@
+﻿// POKEDEXX/Services/PokeApiService.cs
+
+using POKEDEXX.Models;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace POKEDEXX.Services;
+
+public class PokeApiService
+{
+    private readonly HttpClient _httpClient;
+
+    public PokeApiService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+        _httpClient.BaseAddress = new System.Uri("https://pokeapi.co/api/v2/");
+    }
+
+    
+    public async Task<PokemonViewModel> GetPokemonAsync(string identifier)
+    {
+       
+        if (string.IsNullOrWhiteSpace(identifier))
+        {
+            return null;
+        }
+
+       
+        var response = await _httpClient.GetAsync($"pokemon/{identifier}");
+
+        if (response.IsSuccessStatusCode)
+        {   
+            var content = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            var pokemon = JsonSerializer.Deserialize<PokemonViewModel>(content, options);
+            return pokemon;
+        }
+
+        return null; 
+    }
+}
